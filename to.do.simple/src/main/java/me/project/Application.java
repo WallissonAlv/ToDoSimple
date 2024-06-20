557f64729eb3662625791260fc7e4175fadf6e7d
WallissonAlv <wallissonlima008@gmail.com>
2024-06-20 15:27:55 -0300

    First commit

---
+++ to.do.simple/src/main/java/me/project/Application.java
@@ -0,0 +1,28 @@
+package me.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/*
+ * Welcome my web application! This API was builded with those configurations
+ * 
+ * Tecnologies:
+ * - Java Developement Kit 1.8 version
+ * - Spring Boot 2.7.12
+ * - Starter Data JPA
+ * - H2 Database
+ * - PostgreSQL
+ * - OpenApi(Swagger)
+ * - Railway
+ * 
+ * @Author WallissonAlv
+ * */
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
